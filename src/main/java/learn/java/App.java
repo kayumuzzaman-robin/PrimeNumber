10fb4c4f7694b31f284841d44a5f465eeae63c9c
kayumuzzaman <kayumuzzaman.robin@gmail.com>
2021-04-06 23:41:07 +0600

    added prime number checking method.

--- src/main/java/learn/java/App.java
+++ src/main/java/learn/java/App.java
@@ -1,13 +1,41 @@
 package learn.java;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
- *
+ * Find Prime Number
+ * If input is not an int value, it will ask for the input again
+ * If input is an int, isPrime() method will be called
+ * & check if it is a prime or not and give the result and terminate the program
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static boolean isPrime(int number) {
+        if (number == 1 || number == 2) {
+            return false;
+        } else if (number > 2) {
+            for (int i = 3; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Find prime number, input a number to check: ");
+        Scanner scanner = new Scanner(System.in);
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input, try again!");
+            scanner.next();
+        }
+
+        int input = scanner.nextInt();
+        System.out.println("Result of isPrime() = " + isPrime(input));
+
     }
 }
